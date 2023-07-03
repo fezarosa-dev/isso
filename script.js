4fa7184eaea1019bfa4b5844f311afddf3441250
fezarosa_dev <130026434+fezarosa-dev@users.noreply.github.com>
2023-07-03 00:31:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const toggleSwitch = document.querySelector('.dn');
+
+toggleSwitch.addEventListener('change', function () {
+    if (this.checked) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+});
